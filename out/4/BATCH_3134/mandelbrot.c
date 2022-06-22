4c6071688502dc123041f1dbb256451ca3452be4
pp12704 <pp12704@lad.pucrs.br>
2022-06-21 22:28:43 -0300

    openmp

---
+++ out/4/BATCH_3134/mandelbrot.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h> 
+#include <omp.h>
+
+struct complex{
+  double real;
+  double imag;
+};
+
+
+int main(int argc, char *argv[]){
+  int i, j, iter, numoutside = 0, total, init, end;
+  double area, error, ztemp;
+  double start, finish;
+  struct complex z, c;
+  // MPI information variables;
+  int id, procnum, size;
+
+   /*
+   *   
+   *
+   *     Outer loops run over npoints, initialise z=c
+   *
+   *     Inner loop has the iteration z=z*z+c, and threshold test
+   */
+  MPI_Status status;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &procnum);
+  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+  if(id == 0) { // MASTER
+    MPI_Barrier(MPI_COMM_WORLD);
+    start = MPI_Wtime();  
+  
+    init = 0;
+    
+    for (i=1; i < procnum; i++) {
+      int end = (i+1) * NPOINTS / procnum;
+      MPI_Send(&init,1,MPI_INT,i,0,MPI_COMM_WORLD);
+      MPI_Send(&end,1,MPI_INT,i,0,MPI_COMM_WORLD);
+      init = end;
+    }
+
+    MPI_Reduce(&numoutside,&total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+
+    /*
+     *  Calculate area and error and output the results
+     */
+
+    finish = MPI_Wtime();  
+    area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-total)/(double)(NPOINTS*NPOINTS);
+    error=area/(double)NPOINTS;
+    fprintf(stderr, "%d\t%lf\n",NPOINTS,finish-start);
+    printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
+    //printf("%d\t%lf\n",NPOINTS,finish-start);
+
+  // --------------------------------------------------
+  } else { //SLAVE
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    MPI_Recv(&init,1,MPI_INT,MPI_ANY_SOURCE,0,MPI_COMM_WORLD,&status);
+    MPI_Recv(&end,1,MPI_INT,status.MPI_SOURCE,0,MPI_COMM_WORLD,&status);
+    
+    #pragma omp parallel for private(i,j,c,z,ztemp,iter) reduction(+:numoutside)
+    for (i=init; i < end; i++) {
+      for (j=0; j<NPOINTS; j++) {
+        c.real = -2.0+2.5*(double)(i)/(double)(NPOINTS)+1.0e-7;
+        c.imag = 1.125*(double)(j)/(double)(NPOINTS)+1.0e-7;
+        z=c;
+        for (iter=0; iter<MAXITER; iter++) {
+          ztemp=(z.real*z.real)-(z.imag*z.imag)+c.real;
+          z.imag=z.real*z.imag*2+c.imag; 
+          z.real=ztemp; 
+          if ((z.real*z.real+z.imag*z.imag)>4.0e0) {
+            numoutside++;
+            break;
+          }
+        }
+      }
+    }
+    
+    MPI_Reduce(&numoutside,&total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+  } 
+
+  MPI_Finalize();
+  return 0;
+}
